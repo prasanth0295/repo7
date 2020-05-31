359badaa52b5d5a77416ad700abb0323e5f153e8
Surya Prasanth Vaddepati <prasanth0295@github.com>
2020-05-31 20:59:51 +0530

    Added Test7.java

--- Repo7/src/Test7.java
+++ Repo7/src/Test7.java
@@ -5,7 +5,7 @@ public class Test7 {
 	public static void main(String[] args) throws InterruptedException {
 		for(int i=0;i<11;i++) {
 			System.out.println("Iteration Number from Test6 class is :"+i);
-			Thread.sleep(2500);
+			Thread.sleep(3000);
 			System.out.println("***************************************************"+new Date().toString());
 		}
 
